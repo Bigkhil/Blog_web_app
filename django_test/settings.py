2d59f2d0136d3128129838fd17849792e76b944f
Mohamed khalil <khm40005000@outlook.com>
2024-08-18 15:26:59 +0300

    made changes in the login and logout pages

--- django_test/settings.py
+++ django_test/settings.py
@@ -126,5 +126,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
