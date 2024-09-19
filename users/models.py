8acc36225fa223797ccadcb5b8516a5d1b816818
Bigkhil <khm40005000@outlook.com>
2024-09-19 17:13:02 +0300

    a lot of changes

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,4 +8,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-# Create your models here.
+    
+    def save(self):
+        #we will overwrite the default save method of the parent class which is Model to resize the saved image
+        
+        super().save() # this line will call the default save method of the parent class
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
